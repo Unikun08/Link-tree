d2e26ffcb914ce320d1ce8e16dc5978f8e25f2a8
Unikun08 <giovanna.almeid4@gmail.com>
2026-04-07 21:48:30 -0300

    att

--- Método SV/js/interatividade.js
+++ Método SV/js/interatividade.js
@@ -17,19 +17,24 @@ var modoescuro = false
 
 botao_modoescuro.click(()=>{
 
-    if( modoescuro == false ) {
+    if(modoescuro == false ){
 
-        botoes.css( "background-color" , "rgb(125, 22, 51)");
-        botoes.css("color" , "white");
+        botoes.css("background-color","rgb(57, 8, 26)");
+        botoes.css("color","white");
 
-        modoescuro = true;
+        fundo.css("background-image","url('https://i.pinimg.com/736x/d6/bd/2d/d6bd2d6d5f9937c1e52e66354a110c8e.jpg')")
+
+        modoescuro = true
+
 
     }else{
 
-        botoes.css( "background-color" , "rgb(57, 8, 26)");
-        botoes.css("color" , "white");
+        botoes.css( "background-color","rgb(125, 22, 51)");
+        botoes.css("color","white");
 
-        modoescuro = false;
+        fundo.css("background-image","url('https://i.pinimg.com/736x/29/ed/80/29ed8045f52712cdd6bfe79318c55802.jpg')")
+
+        modoescuro = false
 
     }
 
